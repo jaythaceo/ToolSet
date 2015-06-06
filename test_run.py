2c9d0752f3c0a9825aab6e18c174c02db672a101
jaythaceo <jaythaceo@gmail.com>
2015-06-05 20:06:59 -0400

    [chore] Added get info function Step1 of requirements

--- test_run.py
+++ test_run.py
@@ -1,8 +1,10 @@
 from bs4 import BeautifulSoup
 import urllib2
 
-target = urllib2.urlopen("http://www.courts.state.ny.us/reporter/3dseries/2015/2015_04674.htm").read()
-soup = BeautifulSoup(target)
-#print type(soup)
+TARGET = "http://www.courts.state.ny.us/reporter/3dseries/2015/2015_04674.htm"
 
-print soup.prettify()
+def get_info(info):
+  html = urllib2.urlopen(info).read()
+  soup = BeautifulSoup(html)
+  paras = soup.find_all("p")
+  return paras
