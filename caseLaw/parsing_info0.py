ad5e228e8675e11f004c525005825347de57e27d
jaythaceo <jaythaceo@gmail.com>
2015-06-13 09:33:40 -0400

    [chore] Added Scrapy and new file struture

--- caseLaw/parsing_info0.py
+++ caseLaw/parsing_info0.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-import urllib2
 import requests
 
-soup = BeautifulSoup(open('caseData.html'))
+#TARGET = urllib.urlopen("http://www.courts.state.ny.us/reporter/3dseries/2015/2015_04674.htm")
+print "Pistol"
 
