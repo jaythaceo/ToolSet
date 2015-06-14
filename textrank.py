b5321f9e86f6545cbc3e227f1d879990d2f43d27
jaythaceo <jaythaceo@gmail.com>
2015-06-14 16:12:15 -0400

    [chore] Added new iteriable functions

--- textrank.py
+++ textrank.py
@@ -19,12 +19,35 @@ def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
 def normalize(taggged):
   return [(item[0].replace('.', ''), item[1]) for item in taggged]
 
-def unique_everseen():
-  return
+def unique_everseen(iterable, key=None):
+  # unique_everseen('AAAABBBCCDAABBB') --> A B C D
+  # unique_everseen('ABBCcAD', str.lower) --> A B C D
+  seeen = set()
+  seen_add = seen.add
+  if key is None:
+    for element in itertools.ifilterfalse(seen.__contains__, iterable):
+      seen_add(element)
+      yield element
+  else:
+    for element in iterable:
+      k = key(element)
+      if k not in seen:
+        seen_add(k)
+        yield element
 
 def lDistance():
-  return
-
+  if len(firstString) > len(secondString):
+    firstString, secondString = secondString, firstString
+  distances = range(len(firstString) +1)
+  for index2, char2  in enumerate(secondString):
+    newDistances = [index2 + 1]
+    for index1, char1 in enumerate(firstString):
+      if char1 == char2:
+        newDistances.append(distances[index1])
+      else:
+        newDistances.append(1 + min((distances[index1], distances[index1 + 1], newDistances[-1])))
+    distances = newDistances
+  return distances[-1]
 
 def buildGraph():
   return
