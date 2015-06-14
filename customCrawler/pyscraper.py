cb9dcf5215922c3bd86c2ba34715fb6159751aea
jaythaceo <jaythaceo@gmail.com>
2015-06-13 20:52:56 -0400

    [chore] Added get_links_domain

--- customCrawler/pyscraper.py
+++ customCrawler/pyscraper.py
@@ -13,3 +13,21 @@ def get_link(html, url):
       link['href'] = link['href'].strip(' ')
       urls.append(urljoin(url, link['href']))
   return urls
+
+def get_links_domain(html, url):
+  """ Return all urls from page that point to same domain """
+  urls = []
+  dom = urlparse(url).hostname
+  for link in BeautifulSoup(html, parseOnlyThese = SoupStrainer('a')):
+    if link.has_key('href'):
+      if link['href']==' ':
+        continue
+
+      link['href'] = link['href'].strip(' ')
+
+      newurl = urljoin(url, link['href'])
+      udom = urlparse(newurl).hostname
+      if udom == dom:
+        urls.append(newurl)
+  return urls
+
