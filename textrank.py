035e383db7a2a86c5b5e415d088f795fb5ba5aad
jaythaceo <jaythaceo@gmail.com>
2015-06-14 15:28:47 -0400

    [chore] Added syntactic functions

--- textrank.py
+++ textrank.py
@@ -3,13 +3,21 @@ import itertools
 from operator import itemgetter
 import networkx
 import os
+import nltk
+"""
+We are going to do the fucking impossible
+with case law.
 
+This text ranking program, will use special
+machine learning algorithms.
+
+"""
 # Apply syntactic filters
-def filter_for_tags():
-  return
+def filter_for_tags(tagged, tags=['NN', 'JJ', 'NNP']):
+  return [item for item in tagged if item[1] in tags]
 
-def normalize():
-  return
+def normalize(taggged):
+  return [(item[0].replace('.', ''), item[1]) for item in taggged]
 
 def unique_everseen():
   return
