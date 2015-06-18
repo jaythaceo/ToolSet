4b2dbc2dedc702b7020228794e2db25ec48630cc
jaythaceo <jaythaceo@gmail.com>
2015-06-18 01:30:46 -0400

    [chore] Added print for some reason

--- caseLaw/parse_data.py
+++ caseLaw/parse_data.py
@@ -5,11 +5,3 @@ data = open('caseData.html', 'r')
 soup = BeautifulSoup(data.read())
 element = soup.find_all('td', {"align": 'center'})
 category = soup.find_all('p')
-
-def stripTags(data):
-  category = data
-  if category is None:
-    return None
-  return category
-
-print stripTags(category)
