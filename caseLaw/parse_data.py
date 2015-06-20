88b6569f1086bddbfcf14acd82864e1f7a3e772c
jaythaceo <jaythaceo@gmail.com>
2015-06-20 03:16:35 -0400

    [chore] Removed files

--- caseLaw/parse_data.py
+++
@@ -1,7 +0,0 @@
-from bs4 import BeautifulSoup
-
-
-data = open('caseData.html', 'r')
-soup = BeautifulSoup(data.read())
-element = soup.find_all('td', {"align": 'center'})
-category = soup.find_all('p')
