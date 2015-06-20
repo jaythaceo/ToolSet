4baef18c374af0c6483e2f97c7b5c8d42a62c7bb
jaythaceo <jaythaceo@gmail.com>
2015-06-20 03:38:51 -0400

    [chore] Added target site and page

--- caseLaw/fetch_data.py
+++ caseLaw/fetch_data.py
@@ -1,10 +1,12 @@
 # imported libraries
 from bs4 import BeautifulSoup
 import urllib
+import re
 
 TARGET = urllib.urlopen("http://www.courts.state.ny.us/reporter/slipidx/aidxtable_1.shtml").read()
 soup = BeautifulSoup(TARGET)
-tags = soup('a')
 
-for tag in tags:
-  print tag.get('href', None)
+"""
+Remove html tags and useless
+text from document.
+"""
